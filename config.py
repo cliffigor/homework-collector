c0a21ca2ce80dea268934b3a97ba1c1e2e50e03b
cliffigor <jitianxu25@gmail.com>
2022-09-17 14:18:40 +0800

    add configuration about uploading

--- config.py
+++ config.py
@@ -9,5 +9,10 @@ class Config(object):
         'DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # 上传的文件夹名称
+    UPLOAD_FOLDER = 'files'
+    # 上传的文件夹路径
+    UPLOAD_PATH = os.path.join(basedir, UPLOAD_FOLDER)
+    # 允许上传的文件格式
     ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg',
                           'gif', 'ppt', 'pptx', 'doc', 'docx', '7z', 'rar', 'zip'}
